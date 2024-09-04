269ade9024865d973f53b15d1519604e4a4110fd
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2024-09-04 09:36:45 +0300

    **Remove unnecessary placeholder and temporary files**
    Deleted various placeholder files from the project directory, including `.gitkeep` files and sample `.env`. Also removed temporary data files from the `pgdata` directory. This cleanup helps maintain a cleaner repository structure.

--- src/api/news-post/controllers/news-post.ts
+++
@@ -1,7 +0,0 @@
-/**
- * news-post controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::news-post.news-post');
--- src/api/news-post/routes/news-post.ts
+++
@@ -1,7 +0,0 @@
-/**
- * news-post router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::news-post.news-post');
